5b9ebd6871dfb6b419c250ee71d97f8c657d79f7
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2018-09-28 13:11:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports =function solveEquation(equation) {
+ 
+var arr = equation.split(" ");
+  
+var a= +arr[0];
+  
+var b= +(arr[3]+arr[4]);
+  
+var c=+(arr[7]+arr[8]);
+ 
+var D = b * b - 4 * a * c;
+
+if (D > 0) {
+
+var x1 = Math.round((-b - Math.sqrt(D)) / (2 * a));
+ 
+var x2= Math.round((-b + Math.sqrt(D)) / (2 * a));
+  
 }
+
+else if (D == 0) {
+ 
+x = Math.round(-b / (2 * a));
+
+}
+
+}
+
