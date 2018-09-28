1f653130c032a34029ec8ccdba4a106c01807b68
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2018-09-28 21:00:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ var D = b * b - 4 * a * c;
 if (D > 0) {
 var x1 = Math.round((-b - Math.sqrt(D)) / (2 * a)); 
 var x2= Math.round((-b + Math.sqrt(D)) / (2 * a));
-console.log(x1);
-console.log(x2);  
 }
 else if (D == 0) { 
-var x = Math.round(-b / (2 * a)); 
-console.log(x);
+var x3 = Math.round(-b / (2 * a)); 
 }
+else (D < 0) { 
+var no = 'Нет решений';
 }
