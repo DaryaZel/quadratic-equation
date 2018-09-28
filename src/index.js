81b1d072e97ba9ae3941a475a27af4f7d7ee799b
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2018-09-28 13:18:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-module.exports =function solveEquation(equation) {
- 
-var arr = equation.split(" ");
-  
-var a= +arr[0];
-  
+module.exports =function solveEquation(equation) { 
+var arr = equation.split(" ");  
+var a= +arr[0];  
 var b= +(arr[3]+arr[4]);
-  
-var c=+(arr[7]+arr[8]);
- 
+var c=+(arr[7]+arr[8]); 
 var D = b * b - 4 * a * c;
-
 if (D > 0) {
-
-var x1 = Math.round((-b - Math.sqrt(D)) / (2 * a));
- 
+var x1 = Math.round((-b - Math.sqrt(D)) / (2 * a)); 
 var x2= Math.round((-b + Math.sqrt(D)) / (2 * a));
-  
+console.log(x1); 
+console.log(x2);
 }
-
-else if (D == 0) {
- 
-x = Math.round(-b / (2 * a));
-
+else if (D == 0) { 
+x = Math.round(-b / (2 * a)); 
+console.log(x);
 }
-
 }
+solveEquation();
 
