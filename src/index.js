3dfdb02efc247666133cc43858ab2eae9332cea1
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2018-09-29 21:03:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 module.exports =function solveEquation(equation) { 
-var arr = equation.split(" ");  
-var a= +arr[0];  
-var b= +(arr[3]+arr[4]);
-var c=+(arr[7]+arr[8]); 
-var D = b * b - 4 * a * c;
-if (D > 0) {
-var x1 = Math.round((-b - Math.sqrt(D)) / (2 * a)); 
-var x2= Math.round((-b + Math.sqrt(D)) / (2 * a));
-}
-else if (D == 0) { 
-var x3 = Math.round(-b / (2 * a)); 
-}
-}
+    var arr = equation.split(" ");  
+    var a= +arr[0];  
+    var b= +(arr[3]+arr[4]);
+    var c=+(arr[7]+arr[8]); 
+    var D = b * b - 4 * a * c;
+    var x1 = Math.round((-b - Math.sqrt(D)) / (2 * a)); 
+    var x2= Math.round((-b + Math.sqrt(D)) / (2 * a));
+      if (x1>x2) {
+        var sol = [];
+        sol[0] = x2;
+        sol[1] = x1;
+      }
+      else if (x1 < x2) {
+        var sol = [];
+        sol[0]=x1;
+        sol[1]=x2;
+      }
+    }
